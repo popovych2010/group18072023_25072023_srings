7a237fdd86080c91c0bc3d2098d47c0e0f55e75c
popovych2010 <o.popovych2010@gmail.com>
2023-07-29 15:36:14 +0200

    intro strings

---
+++ strings.py
@@ -0,0 +1,20 @@
+my_cat = 'My cat'
+
+my_greeting = "I'm David"
+my_greeting2 = 'I\'m \nDavid and \tI\'m a man!!!'
+print(my_greeting2)
+eneida_chunk = """
+
+    Еней був парубок моторный \U000026F7
+"""
+# print(eneida_chunk)
+#
+# myltiplicated = my_cat * 15
+# print(myltiplicated)
+# print('*' * 150)
+
+summa = my_greeting + my_cat
+print(summa)
+
+skier_unicode = '\U000026F7'
+print(skier_unicode)
